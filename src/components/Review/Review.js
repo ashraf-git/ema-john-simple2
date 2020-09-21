a48fad356ee161fa871e47a8402989075c54db07
ashraf-git <aliashraf.hena@gmail.colm>
2020-09-21 09:04:24 +0600

    routing

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { getDatabaseCart } from '../../utilities/databaseManager';
 import fakeData from '../../fakeData';
+import ReviewItem from '../ReviewItem/ReviewItem';
 
 const Review = () => {
-    const {cart, setCart} = useState([]);
+    const [cart, setCart] = useState([]);
     useEffect(() =>{
-        //cart
-        const savedCart = getDatabaseCart;
+        const savedCart = getDatabaseCart();
         const productKeys = Object.keys(savedCart)
         const cartProducts = productKeys.map(key => {
-            const proudct = fakeData.find(pd = pd.key === key)
+            const proudct = fakeData.find(pd => pd.key === key)
             product.quantity = savedCart[key];
             return product;
         },[]);
@@ -17,7 +17,10 @@ const Review = () => {
     })
     return (
         <div>
-            <h1>cart Iems: cart.length</h1>
+            <h1>cart Iems: {cart.length}</h1>
+            {
+                cart.map(pd => <ReviewItem product={pd}></ReviewItem> )
+            }
         </div>
     );
 };
